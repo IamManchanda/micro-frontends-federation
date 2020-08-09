200125aac618a0082f6828e17b6024d2a36c14fa
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-08-10 01:21:28 +0530

    Exposing Our Components

--- Form/webpack.common.js
+++ Form/webpack.common.js
@@ -1,16 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
-const htmlConfig = {
-  title: "Webpack 5",
-  chunks: ["main"],
-  filename: "index.html",
-  inject: "body",
-  favicon: "favicon.ico",
-  meta: { viewport: "width=device-width,initial-scale=1" },
-  template: "./src/template.html",
-};
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -39,5 +30,44 @@ module.exports = {
       },
     ],
   },
-  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin(htmlConfig)],
+  plugins: [
+    new ModuleFederationPlugin({
+      library: {
+        type: "var",
+        name: "form",
+      },
+      name: "form",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Form": "./src/Form",
+      },
+      remotes: {
+        App: "App",
+      },
+      shared: {
+        "@material-ui/core": {
+          eager: true,
+          singleton: true,
+        },
+        react: {
+          eager: true,
+          singleton: true,
+        },
+        "react-dom": {
+          eager: true,
+          singleton: true,
+        },
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Webpack 5",
+      chunks: ["main"],
+      filename: "index.html",
+      inject: "body",
+      favicon: "favicon.ico",
+      meta: { viewport: "width=device-width,initial-scale=1" },
+      template: "./src/template.html",
+    }),
+  ],
 };
